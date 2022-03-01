5f081d7a20ff4e128c3dbad64cdb75def56a2ad5
Marko Savic <marko.s.savic@saga.rs>
2022-03-01 09:35:24 +0100

    Add gitignore, prettier, readme and counter example

--- README.md
+++ README.md
@@ -1,1 +1,17 @@
 # ELM-TS Base Project
+
+A porting of [The Elm Architecture](https://guide.elm-lang.org/architecture/) to TypeScript featuring fp-ts, RxJS and React.
+
+Original project by: [Gulio Canti elm-ts](https://github.com/gcanti/elm-ts)
+
+## Installation
+
+```
+yarn install
+```
+
+## Start project
+
+```
+yarn start
+```
--- src/counter/index.tsx
+++ src/counter/index.tsx
@@ -1,4 +1,4 @@
-import { Html }  from 'elm-ts/lib/React';
+import { Html } from 'elm-ts/lib/React'
 import * as Cmd from 'elm-ts/lib/Cmd'
 
 // --- Model
@@ -7,22 +7,28 @@ export type Model = number
 export const init: [Model, Cmd.Cmd<Msg>] = [0, Cmd.none]
 
 // --- Messages
-export type Msg = { type: 'Increment' }
+export type Msg = { type: 'Increment' } | { type: 'Decrement' } | { type: 'Reset' }
 
 // --- Update
 export const update = (msg: Msg, model: Model): [Model, Cmd.Cmd<Msg>] => {
   switch (msg.type) {
     case 'Increment':
       return [model + 1, Cmd.none]
+    case 'Decrement':
+      return [model - 1, Cmd.none]
+    case 'Reset':
+      return [0, Cmd.none]
   }
 }
 
 // --- View
 export const view = (model: Model): Html<Msg> => {
-  return dispatch => (
+  return (dispatch) => (
     <div>
       Count: {model}
       <button onClick={() => dispatch({ type: 'Increment' })}>+</button>
+      <button onClick={() => dispatch({ type: 'Decrement' })}>-</button>
+      <button onClick={() => dispatch({ type: 'Reset' })}>Reset</button>
     </div>
   )
 }
